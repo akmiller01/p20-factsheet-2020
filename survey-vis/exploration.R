9a48b62dea8b1ad3f31ac3122d3c1f3ec15318f6
Alex Miller <alex.k.miller@gmail.com>
2020-09-10 16:15:19 -0400

    Survey exploration

---
+++ survey-vis/exploration.R
@@ -0,0 +1,8 @@
+list.of.packages <- c("data.table","jsonlite","ggplot2","scales")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+setwd("~/git/p20-factsheet-2020/survey-vis")
+
+load("historical_dhs-mics.RData")
