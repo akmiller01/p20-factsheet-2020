1fe8330f2901fd4164ca15474f6e654fcf6daf97
Alex Miller <alex.k.miller@gmail.com>
2020-09-15 12:53:40 -0400

    survey exploration

--- survey-vis/exploration.R
+++ survey-vis/exploration.R
@@ -7,13 +7,20 @@ setwd("~/git/p20-factsheet-2020/survey-vis")
 
 load("historical_dhs-mics_handwashing.RData")
 
+available.vars = names(dhs.mics)
+av = data.frame(var.name=available.vars, var.description=NA)
+fwrite(av, "available_vars.csv")
 
 wash = subset(dhs.mics,handwashing!=-1)
 sleep = subset(dhs.mics,sleeping!=-1 & sleeping!="Inf")
 sleep$sleeping = as.numeric(sleep$sleeping)
+sleep.tab = data.table(sleep)[,.(sleeping=mean(sleeping,na.rm=T)),by=.(p20)]
 
 ggplot(sleep,aes(x=sleeping, group=p20, color=p20)) +
   geom_density() +
+  theme_classic()
+ggplot(sleep.tab,aes(y=sleeping, x=p20)) +
+  geom_bar(stat='identity') +
   theme_classic()
 ggplot(wash,aes(group=handwashing, fill=handwashing, x=p20)) +
   geom_bar(position="fill") +
