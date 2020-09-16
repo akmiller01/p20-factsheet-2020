3588d92b69294d5c8f6df2dca29633b0b976508f
Alex Miller <alex.k.miller@gmail.com>
2020-09-16 08:35:08 -0400

    Guidance on interpretation

--- survey-vis/exploration.R
+++ survey-vis/exploration.R
@@ -7,22 +7,24 @@ setwd("~/git/p20-factsheet-2020/survey-vis")
 
 load("historical_dhs-mics_handwashing.RData")
 
-available.vars = names(dhs.mics)
-av = data.frame(var.name=available.vars, var.description=NA)
-fwrite(av, "available_vars.csv")
+# available.vars = names(dhs.mics)
+# av = data.frame(var.name=available.vars, var.description=NA)
+# fwrite(av, "available_vars.csv")
 
 wash = subset(dhs.mics,handwashing!=-1)
 sleep = subset(dhs.mics,sleeping!=-1 & sleeping!="Inf")
 sleep$sleeping = as.numeric(sleep$sleeping)
-sleep.tab = data.table(sleep)[,.(sleeping=mean(sleeping,na.rm=T)),by=.(p20)]
 
-ggplot(sleep,aes(x=sleeping, group=p20, color=p20)) +
-  geom_density() +
+wash.tab = data.table(wash)[,.(value=sum(value,na.rm=T)),by=.(handwashing,p20,povcal_year)]
+sleep.tab = data.table(sleep)[,.(value=sum(value,na.rm=T)),by=.(sleeping,p20,povcal_year)]
+
+ggplot(sleep.tab,aes(x=sleeping, y=value, group=p20, color=p20)) +
+  geom_line() +
+  facet_wrap(~povcal_year) +
   theme_classic()
-ggplot(sleep.tab,aes(y=sleeping, x=p20)) +
-  geom_bar(stat='identity') +
-  theme_classic()
-ggplot(wash,aes(group=handwashing, fill=handwashing, x=p20)) +
-  geom_bar(position="fill") +
+wash.tab = subset(wash.tab,p20!="(P20) No data" & povcal_year<2019)
+ggplot(wash.tab,aes(group=handwashing, fill=handwashing, x=p20, y=value)) +
+  geom_bar(stat="identity",position="fill") +
   scale_y_continuous(labels=percent) +
+  facet_wrap(~povcal_year) +
   theme_classic()
