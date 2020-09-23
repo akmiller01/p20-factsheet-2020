0ab9ee1cef9629aed38cef4f363a157f6f36930d
Alex Miller <alex.k.miller@gmail.com>
2020-09-23 10:59:35 -0400

    Survey vis lines

--- covid-projections/plotly_test.R
+++ covid-projections/plotly_test.R
@@ -11,7 +11,14 @@ names(all) = make.names(names(all))
 # all[is.na(all)] <- NaN
 
 fig <- plot_ly(all, x = ~year, y = ~Pre.Covid.measured.extreme.poverty, name = 'Pre-Covid measured extreme poverty', type = 'scatter', mode = 'lines',
-               line = list(color = 'rgb(232, 68, 57)', width = 4), connectgaps = TRUE, text = ~paste('Country: ', name)) 
+               line = list(color = 'rgb(232, 68, 57)', width = 4), connectgaps = TRUE, text = ~paste('Country: ', name),transforms = list(
+                 list(
+                   type = 'filter',
+                   target = ~name,
+                   operation = '=',
+                   value = unique(all$name)[1]
+                 )
+               )) 
 fig <- fig %>% add_trace(y = ~Pre.Covid.estimated.extreme.poverty, name = 'Pre-Covid estimated extreme poverty',
                 line = list(color = 'rgb(232, 68, 57)', width = 4, dash = 'dash'), connectgaps = TRUE) 
 fig <- fig %>% add_trace(y = ~Post.Covid.measured.extreme.poverty, name = 'Post-Covid measured extreme poverty',
---
+++ survey-vis/concat.R
@@ -0,0 +1,88 @@
+list.of.packages <- c("data.table","jsonlite","ggplot2","scales")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+setwd("~/git/p20-factsheet-2020/survey-vis")
+
+wash.tab1 = fread("p20_handwashing.csv")
+wash.tab2 = fread("p20_urban_handwashing.csv")
+wash.tab3 = fread("p20_sex_handwashing.csv")
+setnames(wash.tab1, "handwashing", "indicator")
+setnames(wash.tab2, "handwashing", "indicator")
+setnames(wash.tab3, "handwashing", "indicator")
+wash.tab1$demographic_category = "Poorest/richest 20% of population"
+wash.tab2$demographic_category = "Poverty status and location"
+wash.tab3$demographic_category = "Poverty status and sex"
+sleeping.tab1 = fread("p20_crowding.csv")
+sleeping.tab2 = fread("p20_urban_crowding.csv")
+sleeping.tab3 = fread("p20_sex_crowding.csv")
+setnames(sleeping.tab1, "sleeping.category", "indicator")
+setnames(sleeping.tab2, "sleeping.category", "indicator")
+setnames(sleeping.tab3, "sleeping.category", "indicator")
+sleeping.tab1$demographic_category = "Poorest/richest 20% of population"
+sleeping.tab2$demographic_category = "Poverty status and location"
+sleeping.tab3$demographic_category = "Poverty status and sex"
+educ.tab1 = fread("p20_educ.csv")
+educ.tab2 = fread("p20_urban_educ.csv")
+educ.tab3 = fread("p20_sex_educ.csv")
+setnames(educ.tab1, "educ.category", "indicator")
+setnames(educ.tab2, "educ.category", "indicator")
+setnames(educ.tab3, "educ.category", "indicator")
+educ.tab1$demographic_category = "Poorest/richest 20% of population"
+educ.tab2$demographic_category = "Poverty status and location"
+educ.tab3$demographic_category = "Poverty status and sex"
+birth.reg.tab1 = fread("p20_birth.reg.csv")
+birth.reg.tab2 = fread("p20_urban_birth.reg.csv")
+birth.reg.tab3 = fread("p20_sex_birth.reg.csv")
+setnames(birth.reg.tab1, "birth.reg", "indicator")
+setnames(birth.reg.tab2, "birth.reg", "indicator")
+setnames(birth.reg.tab3, "birth.reg", "indicator")
+birth.reg.tab1$demographic_category = "Poorest/richest 20% of population"
+birth.reg.tab2$demographic_category = "Poverty status and location"
+birth.reg.tab3$demographic_category = "Poverty status and sex"
+
+dat = rbindlist(
+  list(
+    wash.tab1,
+    wash.tab2,
+    wash.tab3,
+    sleeping.tab1,
+    sleeping.tab2,
+    sleeping.tab3,
+    educ.tab1,
+    educ.tab2,
+    educ.tab3,
+    birth.reg.tab1,
+    birth.reg.tab2,
+    birth.reg.tab3
+  ),
+  fill=T
+)
+
+
+keep = c(
+  "indicator",
+  "demographic",
+  "demographic_category",
+  "iso3",
+  "povcal_year",
+  "percentage"
+)
+
+dat = dat[,keep,with=F]
+
+codes = fread("/home/alex/git/poverty_predictions/output/globalproj_long_Apr20.csv")
+codes = subset(codes,!endsWith(DisplayName,"-Rural") & !endsWith(DisplayName,"-Urban"))
+codes = unique(codes[,c("DisplayName","CountryCode")])
+names(codes) = c("name","iso3")
+
+dat = merge(dat,codes,all.x=T,by="iso3")
+dat$indicator[which(dat$indicator=="no soap and water")] = "No access to soap and water"
+dat$indicator[which(dat$indicator=="5 people per room or more")] = "Crowded living conditions"
+dat$indicator[which(dat$indicator=="Registered")] = "Birth registered"
+
+setnames(dat,"povcal_year","year")
+setnames(dat,"percentage","value")
+
+fwrite(dat,"all_survey.csv")
